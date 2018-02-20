8e66c77229439922dce53e0abf473818679aa8df
Zsolt Varga <zsoltvarga@Zsolts-MacBook-Pro.local>
2018-02-20 19:02:47 +0000

    Design document cipher skeleton added, minor cleaning up

--- assets/js/designDoc.js
+++ assets/js/designDoc.js
@@ -17,3 +17,26 @@ function drawClips(){
     clips[i].style.margin = "0 " + Math.floor((clipHolder.offsetWidth-(numOfClips*10))/numOfClips/2) + "px";
   }
 }
+
+var arrowLeft = document.getElementById('toPlaintext');
+var arrowRight = document.getElementById('toCypher');
+var inputPlain = document.getElementsByTagName('textarea').item(1);
+var inputCypher = document.getElementsByTagName('textarea').item(2);
+
+inputPlain.addEventListener("focusin", function(){
+  arrowLeft.classList.add("dimDocArrow");
+});
+
+inputCypher.addEventListener("focusin", function(){
+  arrowRight.classList.add("dimDocArrow");
+});
+
+inputPlain.addEventListener("focusout", function(){
+  arrowRight.classList.remove("dimDocArrow");
+  arrowLeft.classList.remove("dimDocArrow");
+});
+
+inputCypher.addEventListener("focusout", function(){
+  arrowRight.classList.remove("dimDocArrow");
+  arrowLeft.classList.remove("dimDocArrow");
+});
